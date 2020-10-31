7cfb4e4629f34aa7be4dad4b7f47dbc57ccc0832
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-10-30 23:14:08 -0300

    feat(animate-svg): create value curve component base

---
+++ src/components/ValueCurve.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CurveWrapper = styled.div``;
+
+function ValueCurve() {
+  return (
+    <CurveWrapper>
+        
+    </CurveWrapper>
+  );
+}
+
+export default ValueCurve;
