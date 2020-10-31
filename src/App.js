cd78fd6608db2398f7ae8ef6ae29114096256adf
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-10-30 23:17:03 -0300

    feat(animate-svg): put component in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ValueCurve from './components/ValueCurve';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Aplicação
+        <ValueCurve/>
       </header>
     </div>
   );
