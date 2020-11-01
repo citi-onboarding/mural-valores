b4f7d092c928e7a7b6252d2aa09972b6bad71384
Ester Prazim <50221141+EsterPrazim@users.noreply.github.com>
2020-10-31 21:39:37 -0300

    fix(button-style): change style

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -3,12 +3,30 @@ import Theme from '../../assets/styles/theme';
 
 const ButtonWrapper = styled.div`
   margin-bottom: 0.5rem;
-`;
-const ButtonInput = styled.input`
-  margin-right: 0.75rem;
-`;
-const ButtonLabel = styled.label`
-  font: ${Theme.font.smallTitle};
+  background-color: #8cba4e;
+  padding: 0.25rem;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export { ButtonWrapper, ButtonInput, ButtonLabel };
+const ButtonInput = styled.input`
+  margin-right: 0.75rem;
+  font-size: 0.9rem;
+`;
+
+const ButtonLabel = styled.label`
+  font-family: sans-serif;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const ButtonIcon = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export { ButtonWrapper, ButtonInput, ButtonLabel, ButtonIcon };
